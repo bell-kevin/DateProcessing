dd0e8ba147ee63233fdfb53c90cfdd96d1725542
Kevin Bell <bellKevin@pm.me>
2022-09-14 10:04:00 -0600

    Add files via upload

---
+++ dateProcessing/src/dateprocessing/Date.java
@@ -0,0 +1,10 @@
+/** ****************************************************************************
+ * DateProcessing.java
+ * Kevin Bell
+ *
+ * This stores date values and prints the date in numeric and alphabetic form
+ **************************************************************************** */
+package dateprocessing;
+
+public class Date {
+} // end Date class
---
+++ dateProcessing/src/dateprocessing/DateProcessing.java
@@ -0,0 +1,13 @@
+/** ****************************************************************************
+ * DateProcessing.java
+ * Kevin Bell
+ *
+ * This driver program changes number months to letter/name months
+ **************************************************************************** */
+package dateprocessing;
+
+public class DateProcessing {
+
+    public static void main(String[] args) {
+    } // end main method  
+} // end DateProcessing class
