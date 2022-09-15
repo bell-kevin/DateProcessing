4adb2d9fe8d2b25c148800b85d54b2ce76446f0a
Kevin Bell <bellKevin@pm.me>
2022-09-14 20:49:48 -0600

    Update Date.java

--- dateProcessing/src/dateprocessing/Date.java
+++ dateProcessing/src/dateprocessing/Date.java
@@ -1,15 +1,42 @@
 /** ****************************************************************************
- * DateProcessing.java
+ * Date.java
  * Kevin Bell
  *
  * This stores date values and prints the date in numeric and alphabetic form
  **************************************************************************** */
 package dateprocessing;
 
-import java.time.LocalDateTime; // Import the LocalDateTime class
-import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
-import java.util.Locale; // Import the Locale class
-import java.util.Scanner; // Import the Scanner class
-
 public class Date {
+
+    int day, month; //instance variables
+    String error = null; //instance variables
+    static final String[] MONTHS = {"January", "February", "March", "April",
+        "May", "June", "July", "August", "September", "October", "November",
+        "December"}; // class constant
+    static final int[] DAYSPERMONTH = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30,
+        31, 30, 31}; // class constant
+//******************************************************************************
+
+    public Date(String dateStr) {
+        int slashIndex;
+        try {
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid format - For input string: ");
+        } // end try/catch exception handling
+    } // end Constructor method
+//******************************************************************************
+
+    public void printResults() {
+
+    } // end printResults method
+//******************************************************************************
+
+    public String getError() {
+        return error;
+    } // end getError method
+
+    public String substring() {
+        return "hello";
+    }
 } // end Date class
