3d12846ccd0807f24c153f2c79e46cb8ecb11dc5
Kevin Bell <bellKevin@pm.me>
2022-09-16 10:06:32 -0600

    Update DateProcessing.java

--- dateProcessing/src/dateprocessing/DateProcessing.java
+++ dateProcessing/src/dateprocessing/DateProcessing.java
@@ -14,14 +14,20 @@ public class DateProcessing {
         System.out.println("Ch 15 Date Processing Project by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         String dateStr;
-        Date date = new Date();
+        Date date;
         do {
             System.out.print("Enter a date in format mm/dd (\"q\" to quit): ");
             dateStr = computerKeyboardInput.next();
             if ("q".equalsIgnoreCase(dateStr)) {
                 break;
             } else {
-                date.Date(dateStr);
+                date = new Date(dateStr);
+                String error = date.getError();
+                if (error == null){
+                    date.printResults();
+               } else {
+                    System.out.println(error);
+                    }
             } // end if/else condition
         } while (!dateStr.equalsIgnoreCase("q")); // end while loop
     } // end main method
