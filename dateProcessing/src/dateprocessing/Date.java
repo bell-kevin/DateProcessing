d0c8523286fd24c58dcb80813c6eb73b7d0d8dc5
Kevin Bell <bellKevin@pm.me>
2022-09-15 20:02:05 -0600

    Update Date.java

--- dateProcessing/src/dateprocessing/Date.java
+++ dateProcessing/src/dateprocessing/Date.java
@@ -8,10 +8,10 @@ package dateprocessing;
 
 public class Date {
 
-    private String dateStr;
+    private String dateStr; // do not delete this variable
     int day, month; //instance variables
     String error = null; //instance variables
-    static final String[] MONTHS = {"January", "February", "March", "April",
+    static final String[] MONTHS = {"", "January", "February", "March", "April",
         "May", "June", "July", "August", "September", "October", "November",
         "December"}; // class constant
     static final int[] DAYSPERMONTH = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30,
@@ -33,6 +33,16 @@ public class Date {
             month = Integer.parseInt(currentStringToProcess);
             currentStringToProcess = dateStr.substring(slashIndex + 1);
             day = Integer.parseInt(currentStringToProcess);
+            if (month > 12 || month < 1) {
+                System.out.println("Invalid month " + month + " for day " + day
+                        + ", try again");
+            } else if (day > DAYSPERMONTH[month] || day < 1) {
+                System.out.println("Invalid day " + day + " for month " + month
+                        + ", try again");
+            } else {
+                System.out.println("Date " + month + "/" + day + " is "
+                        + MONTHS[month] + " " + day);
+            }// end if/else if/else conditions
         } catch (NumberFormatException e) {
             System.out.println("Invalid format - For input string: "
                     + "\"" + currentStringToProcess + "\"");
